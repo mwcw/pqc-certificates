62893502457f6e58633af0c3c1069f21fe21eaa6
mwcw <megan@cryptoworkshop.com>
2024-08-28 16:47:43 +1000

    self homing script

--- providers/bc/verify_r3.sh
+++ providers/bc/verify_r3.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 set -e
 
-betas=https://www.bouncycastle.org/betas
 
+cd $SCRIPT_DIR
 
 base=`cat lib/beta.ver`
-
 
 javac -d classes -cp lib/bcprov-${base}.jar:lib/bcutil-${base}.jar:lib/bcpkix-${base}.jar src/main/java/*.java
 
